c97f29ca9280f8f0e33eef664c63669b8778a07b
Jeongseok Lee <jslee02@gmail.com>
2015-03-21 03:16:27 -0400

    Add script for install test

--- ci/script_linux.sh
+++ ci/script_linux.sh
@@ -1,6 +1,12 @@
- make
- sudo ldconfig --verbose # So the test executeables can detect libtinyxml2
- if [ $BUILD_CORE_ONLY = OFF ]; then make test; fi
- # cd tools/
- # ./code_check.sh
- # ./abi_check.sh 4.1.0  # Check with DART 4.1.0
+make
+sudo ldconfig --verbose # So the test executeables can detect libtinyxml2
+if [ $BUILD_CORE_ONLY = OFF ]; then make test; fi
+sudo make install
+# cd tools/
+# ./code_check.sh
+# ./abi_check.sh 4.1.0  # Check with DART 4.1.0
+
+# Install dart-example to see DART is installed correctly
+cd ~
+git clone git@github.com:dartsim/dart-examples.git
+(cd dart-examples; mkdir build; cd build; cmake ..; make)
