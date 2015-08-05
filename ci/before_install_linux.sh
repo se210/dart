7877476e0e1895479703a03b446817456169caba
Jeongseok Lee <jslee02@gmail.com>
2015-08-04 20:55:06 -0400

    Add more addons.apt.sources

--- ci/before_install_linux.sh
+++ ci/before_install_linux.sh
@@ -13,22 +13,5 @@ before_install() {
 wget --quiet -O libeigen3-dev_3.2.1-1~precise1_all.deb http://packages.yade-dem.org/precise/libeigen3-dev_3.2.1-1~precise1_all.deb
 sudo dpkg -i libeigen3-dev_3.2.1-1~precise1_all.deb
 
-# Install boost 1.55 compatible with C++11
-wget --quiet -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
-tar -xzf boost_1_55_0.tar.gz
-cd boost_1_55_0/
-sudo apt-get -qq update
-sudo apt-get -qq install build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev 
-./bootstrap.sh --prefix=/usr/local &>/dev/null  # mute the output
-n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
-sudo ./b2 --with=all -j $n install &>/dev/null  # mute the output
-sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/local.conf'
-sudo ldconfig
-
-sudo add-apt-repository --yes ppa:libccd-debs/ppa
-sudo add-apt-repository --yes ppa:fcl-debs/ppa
-sudo add-apt-repository --yes ppa:dartsim/ppa
-sudo apt-get -qq update
-
 (before_install)
 
